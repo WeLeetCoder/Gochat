b98a1d76949769d98dfee38d0661a5da16a2275c
zhongqi <zq2640710087@gmail.com>
2019-05-25 15:59:00 +0800

    add verdor direction

--- config/config.go
+++ config/config.go
@@ -1,15 +1,17 @@
 package config
 
-const MaxUserNum = 60
-const MaxChatRoom = 10
-const MaxTokenTime = 300 // minute
-const ServerPort = ":5000"
-const MongoHost = "mongodb://localhost:27017"
-const MongoDatabase = "GoChat"
-const MongoTokenTab = "token"
-const Mode = 0
-const TokenSecureCode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~|<>?/';:"
-const BroadcastName = "GoChat"
+const (
+	MaxUserNum      = 60
+	MaxChatRoom     = 10
+	MaxTokenTime    = 300 // minute
+	ServerPort      = ":5000"
+	MongoHost       = "mongodb://localhost:27017"
+	MongoDatabase   = "GoChat"
+	MongoTokenTab   = "token"
+	Mode            = 0
+	TokenSecureCode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~|<>?/';:"
+	BroadcastName   = "GoChat"
+)
 
 const (
 	DevMode = iota
